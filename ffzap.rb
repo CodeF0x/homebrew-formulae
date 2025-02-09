961b1b63c3c0e30a7e5de61d9742cee668439949
CodeF0x <contact@codef0x.dev>
2025-02-09 17:02:33 +0100

    feat: ffzap version 1.0.0

--- ffzap.rb
+++ ffzap.rb
@@ -1,21 +1,21 @@
 class Ffzap < Formula
     desc "A multithreaded CLI for digital media processing using ffmpeg. If ffmpeg can do it, ffzap can do it - as many files in parallel as your system can handle. "
     homepage "https://github.com/CodeF0x/ffzap"
-    version "0.3.2"
+    version "1.0.0"
   
     on_macos do
       if Hardware::CPU.arm?
-        url "https://github.com/CodeF0x/ffzap/releases/download/0.3.2/ffzap-macos-aarch64.tar.gz"
-        sha256 "7c3e0f6b9f3c41b7d30913e61c865d6e668d4da22ac5b4942f01fed31b240cc8"
+        url "https://github.com/CodeF0x/ffzap/releases/download/1.0.0/ffzap-macos-aarch64.tar.gz"
+        sha256 "67f6e20a40b050b1c44e4ca491ab8c5272dc8c4ac9d30c561a72eaedb1e04828"
       elsif Hardware::CPU.intel?
-        url "https://github.com/CodeF0x/ffzap/releases/download/0.3.2/ffzap-macos-x86_64.tar.gz"
-        sha256 "175fe94d98793add1ff48ac43a5c625653f1da151638f35a6e7ba9e863f16ae6"
+        url "https://github.com/CodeF0x/ffzap/releases/download/1.0.0/ffzap-macos-x86_64.tar.gz"
+        sha256 "d6a348c5b98fe9d55a23587ca59ea7fec3d268ce31ab59e3aa05ed338e3a9798"
       end
     end
   
     on_linux do
-      url "https://github.com/CodeF0x/ffzap/releases/download/0.3.2/ffzap-linux-x86_64.tar.gz"
-      sha256 "eedb1bcfa20e85d4cc8544baa7c6528a61073b00dd96129517b95d5a6e825a5a"
+      url "https://github.com/CodeF0x/ffzap/releases/download/1.0.0/ffzap-linux-x86_64.tar.gz"
+      sha256 "466681f3f3bf32cd45ea0075430b63ed552b46906d3c0ae659f25df50ed002a8"
     end
   
     def install
